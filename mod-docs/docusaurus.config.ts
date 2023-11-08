d434591a1a0382b911d397c32889baa4bfbf877a
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2023-11-08 11:10:13 +0000

    why

--- mod-docs/docusaurus.config.ts
+++ mod-docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://www.etggames.com",
+  url: "https://yomg2.etggames.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "mod-docs",
