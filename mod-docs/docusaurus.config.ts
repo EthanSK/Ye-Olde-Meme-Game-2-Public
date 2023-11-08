13615f1eeedc8d13f0c5bc68b50c41d37c5653c8
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2023-11-08 11:08:23 +0000

    url path

--- mod-docs/docusaurus.config.ts
+++ mod-docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://www.etggames.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "Ye-Olde-Meme-Game-2-Public",
+  baseUrl: "mod-docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
